5dc7f68ab10f6732ed183f8e0b71729025808d6c
Kobi <pingkobi@gmail.com>
2014-04-22 00:55:33 -0700

    Returned application.html.erb. In 3 views: left provide(:title, 'foo'), left content inside individual <body> tags, Removed redundant HMTL from 3 views.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Static pages (this is a string, for description)" do
 
         it "should have the title 'Home'" do
             visit '/static_pages/home'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Welcome, Home Page")
+            expect(page).to have_title("Ruby on Rails Tutorial Sample App2 | Welcome, Home Page")
         end
     end
 
@@ -25,7 +25,7 @@ describe "Static pages (this is a string, for description)" do
 
         it "should have the title 'Help'" do
             visit '/static_pages/help'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+            expect(page).to have_title("Ruby on Rails Tutorial Sample App2 | Help")
         end        
     end
 
@@ -38,7 +38,7 @@ describe "Static pages (this is a string, for description)" do
 
         it "should have the title 'About Us'" do
             visit '/static_pages/about'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+            expect(page).to have_title("Ruby on Rails Tutorial Sample App2 | About Us")
         end        
     end    
 end
