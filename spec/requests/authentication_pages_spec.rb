559bff8c50bb53160bc84ea0b4da635b6fd8b801
Kobi <pingkobi@gmail.com>
2014-07-16 08:30:29 -0700

    All Authentication & authorization spec passes.  In total, 63 passing rspec tests pass.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -43,23 +43,40 @@ describe "Authentication" do
         end
     end
 
-    # describe "authorization" do
+    describe "authorization" do
 
-    #     describe "for NON-signed-in users" do
-    #         let(:user) {FactoryGirl.create(:user)}
+        describe "for NON-signed-in users" do
+            let(:user) {FactoryGirl.create(:user)}
 
-    #         describe "in the User controller" do
+            describe "in the User controller" do
 
-    #             describe "visiting the edit page" do
-    #                 before { visit edit_user_path(user) }
-    #                 it { should have_title('Sign in') }
-    #             end
+                describe "visiting the edit page" do
+                    before { visit edit_user_path(user) }
+                    it { should have_title('Sign in') }
+                end
 
-    #             describe "submitting to the update action" do
-    #                 before { patch user_path(user) }  # issues HTTP request directly to /users/1 (not using Capybara)
-    #                 specify { expect(response).to redirect_to(signin_path) }
-    #             end
-    #         end
-    #     end
-    # end
+                describe "submitting to the update action" do
+                    before { patch user_path(user) }  # issues HTTP request directly to /users/1 (not using Capybara)
+                    specify { expect(response).to redirect_to(signin_path) }
+                end
+            end
+        end
+
+        describe "as wrong user" do
+            let(:user) { FactoryGirl.create(:user) }
+            let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+            before {sign_in user, no_capybara: true }
+
+            describe "submitting a GET request to Users#edit action" do
+                before { get edit_user_path(wrong_user) }
+                specify { expect(response.body).not_to match(full_title('Edit user')) }
+                specify { expect(response).to redirect_to(root_url) }
+            end
+
+            describe "submitting a PATCH request to the User#update action" do
+                before { patch user_path(wrong_user) }
+                specify { expect(response).to redirect_to(root_url) }
+            end            
+        end
+    end
 end
