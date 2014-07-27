ffd7f0a525718c5bdc38e5bacc47ee891c8961a1
Kobi <pingkobi@gmail.com>
2014-07-27 01:30:35 -0700

    micropost resource and controller made. 'create' and 'destroy' actions created in the controller, access limited to 'signed_in_user' (can't actually create yet b/c no web form for user to enter data).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,13 +53,6 @@ class UsersController < ApplicationController
 
     #BEFORE FILTERS
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
